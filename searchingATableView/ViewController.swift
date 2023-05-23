130cdc92899ad8b72e68cb43ab398e323d726d86
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-05-23 11:26:09 +0200

    first commit

--- searchingATableView/ViewController.swift
+++ searchingATableView/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var name: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
 }
 
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+       guard let Cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? TableViewCell
+        else {return UITableViewCell()}
+        
+        Cell.name.text = "Nande is an Android GURU "
+        
+        return Cell
+    }
+    
+}
